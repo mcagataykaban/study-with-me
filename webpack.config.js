605f134cf3895c1d82ca10c1735a8e9a32a7825c
cagatay kaban <mcagataykaban@gmail.com>
2021-08-18 00:45:30 +0300

    :wrench: update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
   },
   module: {
     rules: [
+      { test: /\.(js)$/, use: "babel-loader", exclude: /node_modules/ },
       {
         test: /\.js$/,
         // exclude: /node_modules/,
